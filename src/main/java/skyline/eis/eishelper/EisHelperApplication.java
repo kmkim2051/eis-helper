3638fa9cefc4ef879a9c879f53dc415155c6566e
Kyungmin-Kim <km.kim3@okestro.com>
2026-07-07 16:29:47 +0900

    init

---
+++ src/main/java/skyline/eis/eishelper/EisHelperApplication.java
@@ -0,0 +1,13 @@
+package skyline.eis.eishelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EisHelperApplication.class, args);
+  }
+
+}
---
+++ src/test/java/skyline/eis/eishelper/EisHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package skyline.eis.eishelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EisHelperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
